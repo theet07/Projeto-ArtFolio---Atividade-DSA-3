016cd2bc429dd995d40c312a84484ef845bd368d
theet07 <matheushow007@gmail.com>
2026-06-29 17:48:17 -0300

    feat(app): conecta rotas estilos globais e scroll to top

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+﻿import { Route, Routes } from 'react-router-dom'
+import Footer from './components/Footer.jsx'
+import Navbar from './components/Navbar.jsx'
+import ScrollToTop from './router/ScrollToTop.jsx'
+import About from './pages/About.jsx'
+import ArtistProfile from './pages/ArtistProfile.jsx'
+import Contact from './pages/Contact.jsx'
+import Gallery from './pages/Gallery.jsx'
+import Home from './pages/Home.jsx'
+import NotFound from './pages/NotFound.jsx'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-stone-50 text-zinc-900">
+      <ScrollToTop />
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/artists/:id" element={<ArtistProfile />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
